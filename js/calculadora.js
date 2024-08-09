eae235952d142ab40ea765e6a236fa06dcc1051a
Miguel Ángel Bernal Valdéz <dm.mbernal@gmail.com>
2024-08-08 20:47:37 -0400

    Refactoring

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,13 +1,27 @@
 document.getElementById('numero_1').focus()
 
-document.getElementById('sumar').addEventListener('click', function() {
-    let numero_1 = parseInt(document.getElementById('numero_1').value)
-    let numero_2 = parseInt(document.getElementById('numero_2').value)
-    document.getElementById('resultado').value = numero_1 + numero_2
-})
+let botones = document.getElementsByTagName('button')
 
-document.getElementById('restar').addEventListener('click', function() {
-    let numero_1 = parseInt(document.getElementById('numero_1').value)
-    let numero_2 = parseInt(document.getElementById('numero_2').value)
-    document.getElementById('resultado').value = numero_1 - numero_2
-})
+for (let indice = 0; indice < botones.length; indice++) {
+    botones[indice].addEventListener('click', function (xthis) {
+        let boton = xthis.target['id']
+        let numero_1 = parseInt(document.getElementById('numero_1').value)
+        let numero_2 = parseInt(document.getElementById('numero_2').value)
+        if(boton === 'sumar'){
+            document.getElementById('resultado').value = numero_1 + numero_2
+            document.getElementById('operacion').innerText = "Sumar"
+        } else if(boton === 'restar'){
+            document.getElementById('resultado').value = numero_1 - numero_2
+            document.getElementById('operacion').innerText = "Restar"
+        } else if(boton === 'multiplicar'){
+            document.getElementById('resultado').value = numero_1 * numero_2
+            document.getElementById('operacion').innerText = "Multiplicar"
+        } else if(boton === 'dividir'){
+            document.getElementById('resultado').value = numero_1 / numero_2
+            document.getElementById('operacion').innerText = "Dividir"
+        } else if(boton === 'modulo'){
+            document.getElementById('resultado').value = numero_1 % numero_2
+            document.getElementById('operacion').innerText = "Modulo"
+        }
+    })
+}
